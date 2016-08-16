311a31d6c76706b2713658dd8bdc25c977114db0
Vitaly Nikolaychuk <vitaly.nikolaychuk@robotsandpencils.com>
2016-08-16 15:55:05 -0600

    Added sample usage of extended browser.

--- extendedbrowser-sample/src/main/java/com/robotsandpencils/extendedbrowsersample/MainActivity.java
+++ extendedbrowser-sample/src/main/java/com/robotsandpencils/extendedbrowsersample/MainActivity.java
@@ -1,13 +1,33 @@
 package com.robotsandpencils.extendedbrowsersample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.robotsandpencils.extendedbrowsersample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        mBinding.toggleAddressBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBinding.webViewBrowser.showAddressBar(!mBinding.webViewBrowser.isAddressBarShowing());
+            }
+        });
+
+        mBinding.toggleNavigationBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBinding.webViewBrowser.showNavigationBar(!mBinding.webViewBrowser.isNavigationBarShowing());
+            }
+        });
     }
 }
