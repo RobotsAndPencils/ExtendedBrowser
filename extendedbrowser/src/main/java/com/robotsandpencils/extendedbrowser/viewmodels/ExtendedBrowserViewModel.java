dcb7ed01508be70d222a31674f0912c5fa0b1108
Vitaly Nikolaychuk <vitaly.nikolaychuk@robotsandpencils.com>
2016-08-18 10:23:31 -0600

    Updated licence headers.

--- extendedbrowser/src/main/java/com/robotsandpencils/extendedbrowser/viewmodels/ExtendedBrowserViewModel.java
+++ extendedbrowser/src/main/java/com/robotsandpencils/extendedbrowser/viewmodels/ExtendedBrowserViewModel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Robots & Pencils, Inc
+ * Copyright (C) 2016 Robots and Pencils, Inc
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE.txt file for details.
